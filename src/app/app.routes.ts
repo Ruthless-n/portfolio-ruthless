0e302418f96d606041fb4427d250322f40cc257c
Ruthless-n <ruthcnovais@outlook.com>
2025-09-09 17:33:02 -0300

    feat: add pages and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Aboutme } from './pages/aboutme/aboutme';
+import { Skills } from './pages/skills/skills';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Aboutme
+    },
+    {
+        path: 'skills',
+        component: Skills
+    }
+];
---
+++ src/app/core/navbar/navbar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+
+}
